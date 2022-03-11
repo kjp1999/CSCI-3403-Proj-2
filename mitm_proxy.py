02974db86eae24be4118cdfd7d6db23fc2fdb1f9
kjp1999 <kjp199911@gmail.com>
2022-03-10 22:05:45 -0700

    update

--- mitm_proxy.py
+++ mitm_proxy.py
@@ -1,11 +1,19 @@
 import http.server
+import urllib.request
 import socketserver
-class Server(http.server.SimpleHTTPRequestHandler):
+import sys
+
+if len(sys.argv) < 3:
+    print('Usage: python3 mitm.py <port> <payload file>')
+    exit(1)
+class MaliciousProxy(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write(b'Sample response')
-port = 8080
-server = socketserver.ThreadingTCPServer(('', port), Server)
+        print('[*] Got request for: {}'.format(self.path))
+        # Your code will go here
+        req = urllib.request(self.path)
+        
+
+port = sys.argv[1]
+server = socketserver.ThreadingTCPServer(('', port), MaliciousProxy)
 print("[*] Serving on port {}".format(port))
 server.serve_forever()
