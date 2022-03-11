d56936afe99f81eb1d8da567452449074d64dbc8
kjp1999 <kjp199911@gmail.com>
2022-03-10 21:02:10 -0700

    bitchin

---
+++ mitm_proxy.py
@@ -0,0 +1,11 @@
+import http.server
+import socketserver
+class Server(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b'Sample response')
+port = 8080
+server = socketserver.ThreadingTCPServer(('', port), Server)
+print("[*] Serving on port {}".format(port))
+server.serve_forever()
