c37c816e1947a18d0656f12c68845fdbc6e8a8fa
kjp1999 <kjp199911@gmail.com>
2022-03-11 20:27:49 -0700

    updates

--- mitm_proxy.py
+++ mitm_proxy.py
@@ -3,6 +3,8 @@ import urllib.request
 import socketserver
 import sys
 
+from aiohttp import Payload
+
 if len(sys.argv) < 3:
     print('Usage: python3 mitm.py <port> <payload file>')
     exit(1)
@@ -10,10 +12,14 @@ class MaliciousProxy(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         print('[*] Got request for: {}'.format(self.path))
         # Your code will go here
-        req = urllib.request(self.path)
-        
-
-port = sys.argv[1]
+        req = urllib.request.Request(self.path)
+        print(req)
+        print("test")
+        req += Payload
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(req)
+port = int(sys.argv[1])
 server = socketserver.ThreadingTCPServer(('', port), MaliciousProxy)
 print("[*] Serving on port {}".format(port))
 server.serve_forever()
